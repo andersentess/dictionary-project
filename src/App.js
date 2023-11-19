8c6a8d1dc1bf04254809b73b4968a2f57c234182
andersentess <andersentess@gmail.com>
2023-11-19 12:12:45 -0700

    created dictionary component with search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
 import logo from "./logo-tessandersen.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/tess-andersen/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tess Andersen
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/andersentess/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
