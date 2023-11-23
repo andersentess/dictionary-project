61f2df97047ad43b0e3fb73dd73acf981b83f139
andersentess <andersentess@gmail.com>
2023-11-22 17:11:21 -0700

    updated api photo integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,23 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePhotosResponse(response) {
+    console.log(response.data.photos);
   }
 
   function search() {
     // documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let photosApiKey = "0ob8f6fd5f71ctf4bb659c63840b18fa";
+    let photosApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photosApiKey}`;
+
+    axios.get(photosApiUrl).then(handlePhotosResponse);
   }
 
   function handleSubmit(event) {
